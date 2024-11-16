8bca9b01955f1f14a882d852052a5071475460ff
munir <smsirajulmonir@gmail.com>
2024-11-16 18:08:52 +0600

    path.join path.dirname explored

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-add(2,3)
+const {a, add } = require('./local-1')
+const {a:a2, add:add2 } = require('./local-2')
+
+const path = require("path")
+
+console.log(path.join("C:/Projects/learning-node/","local-1.js"));
